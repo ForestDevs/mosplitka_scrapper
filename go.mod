55d32a133120a165b5717465a46d72d85531af2f
dbormotov <dbormotov@direct-credit.ru>
2022-12-17 01:44:55 +0300

    edits

--- go.mod
+++ go.mod
@@ -14,8 +14,8 @@ require (
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.3 // indirect
@@ -24,7 +24,7 @@ require (
 	github.com/xuri/efp v0.0.0-20220603152613-6918739fd470 // indirect
 	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
 	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
-	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.24.0 // indirect
@@ -38,10 +38,10 @@ require (
 	github.com/gocolly/colly/v2 v2.1.0
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/k0kubun/pp v3.0.1+incompatible
 	github.com/mafredri/cdp v0.32.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rs/zerolog v1.26.1 // indirect
+	github.com/takuoki/clmconv v1.1.0
 	github.com/wI2L/jettison v0.7.4 // indirect
 	github.com/xuri/excelize/v2 v2.6.1
 	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
--- utils/excel.go
+++ utils/excel.go
@@ -1,8 +1,95 @@
 package utils
 
 import (
+	"fmt"
+	"log"
 	"mosplitka-parser/models"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/takuoki/clmconv"
+
+	"github.com/xuri/excelize/v2"
 )
 
+func productsSheet(f *excelize.File, products []models.Product) {
+	var titles []string = []string{"Название", "Цена", "Картинки"}
+	var features map[string]int = make(map[string]int)
+	index := f.NewSheet("Товары")
+	f.SetActiveSheet(index)
+	productIndex := 2
+	lastIndex := 0
+
+	for i, title := range titles {
+		f.SetCellValue("Товары", clmconv.Itoa(i)+"1", title)
+		lastIndex = i
+	}
+
+	lastIndex++
+
+	for _, proproduct := range products {
+		f.SetCellValue("Товары", clmconv.Itoa(0)+strconv.Itoa(productIndex), proproduct.Name)
+		f.SetCellValue("Товары", clmconv.Itoa(1)+strconv.Itoa(productIndex), proproduct.Price)
+		f.SetCellValue("Товары", clmconv.Itoa(2)+strconv.Itoa(productIndex), strings.Join(proproduct.Images, ";"))
+		for k, v := range proproduct.Features {
+			_, ok := features[k]
+			if !ok {
+				f.SetCellValue("Товары", clmconv.Itoa(lastIndex)+"1", k)
+				features[k] = lastIndex
+				lastIndex++
+			}
+			f.SetCellValue("Товары", clmconv.Itoa(features[k])+strconv.Itoa(productIndex), v)
+		}
+		productIndex++
+	}
+}
+
+func collectionSheet(f *excelize.File, collection models.Collection) {
+	var titles []string = []string{"Название", "Цена", "Картинки"}
+	index := f.NewSheet("Коллекция")
+	f.SetActiveSheet(index)
+	lastIndex := 0
+
+	for i, title := range titles {
+		f.SetCellValue("Коллекция", clmconv.Itoa(i)+"1", title)
+
+		if title == "Название" {
+			f.SetCellValue("Коллекция", clmconv.Itoa(i)+"2", collection.Name)
+		}
+
+		if title == "Цена" {
+			f.SetCellValue("Коллекция", clmconv.Itoa(i)+"2", collection.Price)
+		}
+
+		if title == "Картинки" {
+			f.SetCellValue("Коллекция", clmconv.Itoa(i)+"2", collection.Image)
+		}
+
+		lastIndex = i
+	}
+
+	lastIndex++
+
+	for k, v := range collection.Features {
+		f.SetCellValue("Коллекция", clmconv.Itoa(lastIndex)+"1", k)
+		f.SetCellValue("Коллекция", clmconv.Itoa(lastIndex)+"2", v)
+		lastIndex++
+	}
+
+	f.SetActiveSheet(index)
+}
+
 func ExcelWrite(collection models.Collection) {
+	f := excelize.NewFile()
+	collectionSheet(f, collection)
+	productsSheet(f, collection.Products)
+	if _, err := os.Stat("./data/"); os.IsNotExist(err) {
+		if err := os.Mkdir("data", os.ModePerm); err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err := f.SaveAs("./data/" + collection.Name + ".xlsx"); err != nil {
+		fmt.Println(err)
+	}
 }
