30772fbc5097c6c99838a8fdf76d1c1a31547969
dbormotov <dbormotov@direct-credit.ru>
2022-12-17 15:08:20 +0300

    add git ignore

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"mosplitka-parser/page"
 	"mosplitka-parser/utils"
 	"time"
@@ -8,12 +10,34 @@ import (
 	"github.com/briandowns/spinner"
 )
 
+type Config struct {
+	Collections []string `json: "collections"`
+}
+
 func main() {
 
-	someSet := []string{"[                    ]", "[===>                  ]", "[=====>                ]", "[=======>              ]", "[========>             ]", "[==========>           ]", "[============>         ]", "[==============>       ]", "[================>     ]", "[==================>   ]", "[====================> ]", "[=====================>]"}
-	s := spinner.New(someSet, 100*time.Millisecond) // Build our new spinner
+	file, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	cfg := Config{}
+
+	if err := json.Unmarshal(file, &cfg); err != nil {
+		panic(err)
+	}
+
+	s := spinner.New(spinner.CharSets[80], 100*time.Millisecond) // Build our new spinner
 	s.Start()
 	c := utils.NewCollector()
-	page.CatalogPlitca(c)
+
+	if len(cfg.Collections) != 0 {
+		for _, url := range cfg.Collections {
+			page.Collection(c, url)
+		}
+	} else {
+		page.CatalogPlitca(c)
+	}
+
 	s.Stop()
 }
